35e086e21c1e0f8e4d8711146c56266966617557
vietdh <viet.dang@dirox.net>
2023-09-27 22:27:39 +0700

    Adding the CRUD methods for AttendeeController

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('events', EventController::class);
 
-Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);
+Route::apiResource('events.attendees', AttendeeController::class)->scoped();
 
