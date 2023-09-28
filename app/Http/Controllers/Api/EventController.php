b0a81c06639d5e1a1ce615e4d6adb599d12232e7
vietdh <viet.dang@dirox.net>
2023-09-28 23:23:57 +0700

    Using Auth Sanctum to guard all routes except index and show

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -12,9 +12,13 @@ class EventController extends Controller
 {
     use CanLoadRelationships;
 
+    public function __construct() {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
     private array $relations = ['user', 'attendees', 'attendees.user'];
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource.2
      */
     public function index()
     {
@@ -37,7 +41,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 895
+            'user_id' => $request->user()->id
         ]);
         return new EventResource($this->loadRelationships($event));
     }
