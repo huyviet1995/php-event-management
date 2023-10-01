15dc95ab35e7ba6a1d712167778c6dcc97dd915b
vietdh <viet.dang@dirox.net>
2023-10-01 22:07:11 +0700

    Add sail (docker) for laravel and configure mailpit

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -34,7 +35,11 @@ class SendEventReminders extends Command
 
         $events->each(
             fn ($event) => $event->attendees->each(
-                fn ($attendee) => $this->info("Notifying the user ($attendee->user_id)")
+                fn ($attendee) => $attendee->user->notify(
+                    new EventReminderNotification(
+                        $event
+                    )
+                )
             )
         );
 
